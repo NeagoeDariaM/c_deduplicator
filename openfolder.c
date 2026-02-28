fcc5cea72a9d29117b04d2d5dd3dc15d7fdb627f
andrei neagoe <daria_maria.neagoe@stud.acs.upb.ro>
2026-02-28 17:58:37 +0200

    Implement directory scanning

---
+++ openfolder.c
@@ -0,0 +1,32 @@
+#include "openfolder.h"
+#include "hash.h"
+#include <stdio.h>
+#include <dirent.h>
+#include <sys/stat.h>
+
+int open_dir(char *directory, file *list, int *count) {
+    struct dirent *new;
+    struct stat st;
+    DIR *dir = opendir(directory);
+    if (dir == NULL) {
+        return -1;
+    }
+    while ((new = readdir(dir)) != NULL) {
+        if (strcmp(new->d_name, ".") == 0 || strcmp(new->d_name, "..") == 0) {
+            continue;
+        }
+        char path[1024];
+        snprintf(path, sizeof(path), "%s/%s", directory, new->d_name);
+        if (stat(path, &st) == 0) {
+            if (S_ISREG(st.st_mode)) {
+                strcpy(list[*count].path, path);
+                strcpy(list[*count].name, new->d_name);
+                list[*count].size = st.st_size;
+                list[*count].hash = dohash(path);
+                (*count)++;
+            }
+        }
+    }
+    closedir(dir);
+    return 0;
+}
